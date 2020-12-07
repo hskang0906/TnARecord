efd84c75224f6726f8337d9835103786783e22f2
hskang0906 <dnfldjaak11@gmail.com>
2020-12-07 18:06:16 +0900

    TnArecord

---
+++ TnARecord/TnARecord_Framework/DBConnector.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using MySql.Data.MySqlClient;
+
+namespace TnARecord
+{
+    public enum DBQueryType
+    {
+        select = 0,
+        insert,
+        update,
+        delete
+    }
+    public class DBConnector
+    {
+        private MySqlConnection DBConnection { get; set; }
+        private MySqlCommand Command { get; set; }
+        private MySqlDataReader reader { get; set; }
+
+        private bool ConnectStatus { get; set; }
+
+        public void Connector(string serverIP = "localhost")
+        {
+            string ConnectString = "Server=localhost;Database=tnarecord;Uid=root;Pwd=root;Charset=utf8;";
+            DBConnection = new MySqlConnection(ConnectString);
+            try
+            {
+                DBConnection.Open();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                ConnectStatus = false;
+            }
+
+            Command = DBConnection.CreateCommand();
+
+        }
+
+        public void Query(DBQueryType querytype, string querystring)
+        {
+            if (DBConnection.Ping() == true)
+            {
+                Command.CommandText = querystring;
+
+                switch (querytype)
+                {
+                    case DBQueryType.select:
+                        reader = Command.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            Console.WriteLine($"{reader["userid"]}, {reader["userpwd"]}, {reader["userName"]}");
+                        }
+                        break;
+                    default:
+                        Command.ExecuteNonQuery();
+                        break;
+                }
+            }
+
+            else
+                Console.WriteLine("DisConnected SQL Server!");
+        }
+    }
+}
---
+++ TnARecord/TnARecord_Framework/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TnARecord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Maria DB Test :: Query");
+
+            var dbconnnector = new DBConnector();
+            dbconnnector.Connector();
+            dbconnnector.Query(DBQueryType.select, "select * from user_info");
+        }
+    }
+}
